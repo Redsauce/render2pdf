d39ce1e18b8ae2cabaf7cbdbfe2bc776a051cb4f
alfonsoabas <alfonsoabas@gmail.com>
2016-12-16 19:22:14 +0100

    If <title> tag does not exists, the filename is "output.pdf"

--- fpm/web/page_title.php
+++ fpm/web/page_title.php
@@ -1,5 +1,6 @@
 <?php
     // function to get the HTML title from a URL
+    // if there not exist the <title> tag, or title is empty, return output
     function page_title($url) {
         $fp = file_get_contents($url);
         if (!$fp)
@@ -7,11 +8,14 @@
 
         $res = preg_match("/<title>(.*)<\/title>/siU", $fp, $title_matches);
         if (!$res)
-            return null;
+            return "output";
 
         // Clean up title: remove EOL's and excessive whitespace.
         $title = preg_replace('/\s+/', ' ', $title_matches[1]);
         $title = trim($title);
+        if $title == ""
+            return "output";
+
         return $title;
     }
 ?>
