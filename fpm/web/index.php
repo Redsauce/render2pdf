16e5e45a48b4d2418072f583c35ccf7c68608d72
alfonsoabas <alfonsoabas@gmail.com>
2016-12-16 18:40:43 +0100

    The PDF file is named with the <title> of the HTML document.

--- fpm/web/index.php
+++ fpm/web/index.php
@@ -1,55 +1,38 @@
 <?php
-
 require 'decoder.php';
-
-$filename = "output.pdf";
-if ($_REQUEST['filename']){
-	if ($_GET['filename']) {
-		$filename = $_GET['filename'];
-	}else{
-		$filename = $_POST['filename'];
-	}
-}
-
+require 'page_title.php';
 if ($_REQUEST['url']) {
-
     $url = "";
-
     if ($_GET['url']) {
         $url = base32_decode($_GET['url']);
     } else {
         $url = $_POST['url'];
     }
 
+
     $random = rand();
-
     $file_url = "/var/www/html/pdfs/output$random.pdf";
-
     set_time_limit(500);
     $command = "wkhtmltopdf -q \"" . $url . "\" $file_url";
     exec($command);
-
     if (file_exists($file_url)) {
         //File Download magic
         header('Content-Type: application/octet-stream');
         header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"".$filename."\"");
+        header("Content-disposition: attachment; filename=\"".page_title($url).".pdf\"");
         readfile($file_url);
         unlink($file_url);
     } else {
         echo "hubo un error! :(";
     }
-
 } else {
     echo '<html>
-        <body>
-        <form action="" method="POST">
+        <body>';
+    echo '<form action="" method="POST">
         <input type="text" name="url" placeholder="http://someplace.com"/>
-	<input type="submit"/></br>
-	<input type="text" name="filename" placeholder="output.pdf"/>
+        <input type="submit"/>
         </form>
         </body>
         </html>';
 }
-
 ?>
