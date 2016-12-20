d59e639f91a5bd5c6214a89955661ce3e2e5b5d7
alfonsoabas <alfonsoabas@gmail.com>
2016-12-20 13:42:41 +0100

    Added the 'filename' field to force the name of the downloaded file

--- containers/fpm/web/index.php
+++ containers/fpm/web/index.php
@@ -2,6 +2,8 @@
 require 'decoder.php';
 require 'page_title.php';
 if ($_REQUEST['url']) {
+
+    // Get the url if exists
     $url = "";
     if ($_GET['url']) {
         $url = base32_decode($_GET['url']);
@@ -9,22 +11,39 @@ if ($_REQUEST['url']) {
         $url = $_POST['url'];
     }
 
+    // Get filename if exist
+    $fileName = "";
+    if ($_GET['filename']) {
+        $fileName = $_GET['filename'];
+    }
+    if ($_POST['filename']) {
+        $fileName = $_POST['filename'];
+    }
 
+    // Get the content in a temporal random file
     $random = rand();
     $file_url = "/var/www/html/pdfs/output$random.pdf";
     set_time_limit(500);
     $command = "wkhtmltopdf -q \"" . $url . "\" $file_url";
     exec($command);
+
+    // If the random file exists
     if (file_exists($file_url)) {
-        //File Download magic
-        $pageTitle = page_title($url);
-        if ($pageTitle == "" || $pageTitle == null)
-            $pageTitle = "output";
+
+        // Set the final fileName
+        if ($fileName == ""){
+            $fileName = page_title($url);
+            if ($fileName == "" || $fileName == null)
+                $fileName = "output";
+        }
+
+        // Create the attachment
         header('Content-Type: application/octet-stream');
         header("Content-Transfer-Encoding: Binary");
-        header("Content-disposition: attachment; filename=\"".$pageTitle.".pdf\"");
+        header("Content-disposition: attachment; filename=\"".$fileName.".pdf\"");
         readfile($file_url);
         unlink($file_url);
+
     } else {
         echo "hubo un error! :(";
     }
@@ -33,7 +52,8 @@ if ($_REQUEST['url']) {
         <body>';
     echo '<form action="" method="POST">
         <input type="text" name="url" placeholder="http://someplace.com"/>
-        <input type="submit"/>
+        <input type="submit"/></br>
+        <input type="text" name="filename" placeholder="output"/>
         </form>
         </body>
         </html>';
