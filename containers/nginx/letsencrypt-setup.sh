64d502f1cc4a1410e34cc529e3eeb34785e8d519
Kenan Rhoton <krhoton@redsauce.net>
2021-06-08 14:51:41 +0200

    Added cron to pdf container for certbot renewals

--- containers/nginx/letsencrypt-setup.sh
+++ containers/nginx/letsencrypt-setup.sh
@@ -3,7 +3,7 @@
 nginx && \
 certbot --nginx --non-interactive --agree-tos --email webmaster@redsauce.net --domains pdf.redsauce.net && \
 killall nginx && sleep 1 && \
-nginx -g "daemon off;"
+crond && nginx -g "daemon off;"
 
 # echo "certbot renew" > /etc/periodic/daily/cerbot-renewal && \
 # chmod +x /etc/periodic/daily/certbot-renewal && \
